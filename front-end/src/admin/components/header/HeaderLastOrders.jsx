d48981a6a208d5ff5e8d1a399bb3b67d800ad52b
Cesar Osses <cesarosses885@gmail.com>
2024-03-07 15:23:43 -0300

    Solucion de error al momento de no haber ordenes por mostrar en el componente HeaderLastOrders

--- front-end/src/admin/components/header/HeaderLastOrders.jsx
+++ front-end/src/admin/components/header/HeaderLastOrders.jsx
@@ -1,11 +1,11 @@
-import { Divider, Dropdown, Button } from 'react-daisyui';
-import { TbBell } from 'react-icons/tb';
+import { Divider, Dropdown, Button, Alert } from 'react-daisyui';
+import { TbBell, TbInfoCircle } from 'react-icons/tb';
 
 import { lastOrders } from '../../data';
 import { HeaderLastOrdersList } from './';
 import { CustomIndicator } from '../custom';
 
-const dataLength = lastOrders.length;
+const dataLength = 0;
 
 export const HeaderLastOrders = () => {
     return (
@@ -13,13 +13,17 @@ export const HeaderLastOrders = () => {
             <Dropdown.Toggle
                 button={false}
                 className='btn btn-circle btn-ghost hover:bg-gray-800 hover:text-gray-50 text-2xl'>
-                {(dataLength !== 0) && <CustomIndicator number={dataLength} element={<TbBell />} />}
+                {(dataLength !== 0) ? <CustomIndicator number={dataLength} element={<TbBell />} /> : <TbBell />}
             </Dropdown.Toggle>
             <Dropdown.Menu className='z-20 mt-10 w-72 max-h-[500px] '>
                 <div className='flex flex-col overflow-y-scroll'>
                     <div className='text-sm text-center font-semibold p-1'>Ultimos Pedidos</div>
                     <Divider className='my-1' />
-                    {(dataLength !== 0) ? <HeaderLastOrdersList lastOrders={lastOrders} /> : <span>No hay ordenes</span>}
+                    {
+                        (dataLength !== 0)
+                            ? <HeaderLastOrdersList lastOrders={lastOrders} />
+                            : <Alert icon={<TbInfoCircle className='text-xl'/>}>No hay pedidos nuevos registrados</Alert>
+                    }
                     {(dataLength > 5) && <Button size='md' className='bg-gray-800 hover:bg-gray-700 text-gray-50 mt-4'>Ver Mas</Button>}
                 </div>
             </Dropdown.Menu>
