ef0513aa2d2d3dbec387db2f9b643063428f0ee0
Cesar Osses <cesarosses885@gmail.com>
2024-03-07 15:32:40 -0300

    Modificacion en componente HeaderLastOrders al en el icono al tener pedidos que mostrar

--- front-end/src/admin/components/header/HeaderLastOrders.jsx
+++ front-end/src/admin/components/header/HeaderLastOrders.jsx
@@ -1,11 +1,10 @@
 import { Divider, Dropdown, Button, Alert } from 'react-daisyui';
-import { TbBell, TbInfoCircle } from 'react-icons/tb';
+import { TbBell, TbInfoCircle, TbBellRinging } from 'react-icons/tb';
 
 import { lastOrders } from '../../data';
 import { HeaderLastOrdersList } from './';
-import { CustomIndicator } from '../custom';
 
-const dataLength = 0;
+const dataLength = lastOrders.length;
 
 export const HeaderLastOrders = () => {
     return (
@@ -13,16 +12,16 @@ export const HeaderLastOrders = () => {
             <Dropdown.Toggle
                 button={false}
                 className='btn btn-circle btn-ghost hover:bg-gray-800 hover:text-gray-50 text-2xl'>
-                {(dataLength !== 0) ? <CustomIndicator number={dataLength} element={<TbBell />} /> : <TbBell />}
+                {(dataLength !== 0) ? < TbBellRinging /> : <TbBell />}
             </Dropdown.Toggle>
             <Dropdown.Menu className='z-20 mt-10 w-72 max-h-[500px] '>
                 <div className='flex flex-col overflow-y-scroll'>
-                    <div className='text-sm text-center font-semibold p-1'>Ultimos Pedidos</div>
+                    <div className='text-sm text-center font-semibold p-1'>Ultimos Pedidos ({dataLength})</div>
                     <Divider className='my-1' />
                     {
                         (dataLength !== 0)
                             ? <HeaderLastOrdersList lastOrders={lastOrders} />
-                            : <Alert icon={<TbInfoCircle className='text-xl'/>}>No hay pedidos nuevos registrados</Alert>
+                            : <Alert icon={<TbInfoCircle className='text-xl' />}>No hay pedidos nuevos registrados</Alert>
                     }
                     {(dataLength > 5) && <Button size='md' className='bg-gray-800 hover:bg-gray-700 text-gray-50 mt-4'>Ver Mas</Button>}
                 </div>
