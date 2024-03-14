267fe486e542b5a8b87b5bcbdae056570e081a3c
cesarom18 <cesarosses885@gmail.com>
2024-03-14 14:26:00 -0300

    Diseño de pagina Error

--- front-end/src/router/AppRouter.jsx
+++ front-end/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { AdminRoutes, AuthRoutes } from './routes';
+import { Error } from '../error/pages';
 
 export const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path='/auth/*' element={<AuthRoutes />} />
-            <Route path='/admin/*' element={<AdminRoutes />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path='/auth/*' element={<AuthRoutes />} />
+      <Route path='/admin/*' element={<AdminRoutes />} />
+      <Route path='/*' element={<Error />} />
+    </Routes>
+  );
 };
