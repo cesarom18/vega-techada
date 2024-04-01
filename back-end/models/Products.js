670069e93e5e2ec51b92cc8b400a369b60620cb7
cesarom18 <cesarosses885@gmail.com>
2024-04-01 12:47:57 -0300

    Creacion de modelo Productos

---
+++ back-end/models/Products.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 60
+    },
+    description: {
+        type: String,
+        minlength: 10,
+        maxlength: 255,
+        required: true,
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'ProductCategories'
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    available: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    offerType: {
+        type: String,
+        enum: ['G', 'KG', 'L', 'ML', 'UN'],
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Products', ProductSchema);
