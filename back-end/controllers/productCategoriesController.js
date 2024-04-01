5c64e1a1f289a15fa60770f00a98449cff6443b2
cesarom18 <cesarosses885@gmail.com>
2024-04-01 19:13:47 -0300

    Incorporacion de endpoints de controlador Estados Orden ademas de modificar la estructura del controlador Categorias Producto

--- back-end/controllers/productCategoriesController.js
+++ back-end/controllers/productCategoriesController.js
@@ -2,6 +2,20 @@ import mongoose from 'mongoose';
 
 import ProductCategories from '../models/ProductCategories.js';
 
+export const getAllProductCategories = async (req, res, next) => {
+    try {
+        const productCategories = await ProductCategories.find();
+        if (productCategories.length === 0) {
+            res.status(404).json({ message: 'No hay categorias de productos registradas' });
+            return next();
+        };
+        res.status(200).json(productCategories);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+        next();
+    };
+};
+
 export const createProductCategory = async (req, res, next) => {
     const productCategory = new ProductCategories(req.body);
     try {
@@ -13,14 +27,20 @@ export const createProductCategory = async (req, res, next) => {
     };
 };
 
-export const getAllProductCategories = async (req, res, next) => {
+export const updateProductCategory = async (req, res, next) => {
     try {
-        const productCategories = await ProductCategories.find();
-        if (productCategories.length === 0) {
-            res.status(404).json({ message: 'No hay categorias de productos registradas' });
+        if (!mongoose.isValidObjectId(req.params.id)) {
+            res.status(404).json({ message: 'El ID otorgado no es valido' });
+            return next()
+        };
+
+        const query = await ProductCategories.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+        if (!query) {
+            res.status(404).json({ message: 'Categoria de producto no encontrada' });
             return next();
         };
-        res.status(200).json(productCategories);
+
+        res.status(200).json({ message: 'Categoria de producto actualizada con exito' });
     } catch (error) {
         res.status(404).json({ message: error.message });
         next();
@@ -41,26 +61,6 @@ export const deleteProductCategory = async (req, res, next) => {
         };
 
         res.status(200).json({ message: 'Categoria de producto eliminada con exito' });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-        next();
-    };
-};
-
-export const updateProductCategory = async (req, res, next) => {
-    try {
-        if (!mongoose.isValidObjectId(req.params.id)) {
-            res.status(404).json({ message: 'El ID otorgado no es valido' });
-            return next()
-        };
-
-        const query = await ProductCategories.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
-        if (!query) {
-            res.status(404).json({ message: 'Categoria de producto no encontrada' });
-            return next();
-        };
-
-        res.status(200).json({ message: 'Categoria de producto actualizada con exito' });
     } catch (error) {
         res.status(404).json({ message: error.message });
         next();
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 const router = express.Router();
 
-import { createProductCategory, getAllProductCategories, deleteProductCategory, updateProductCategory } from '../controllers/productCategoriesController.js';
+import { getAllProductCategories, createProductCategory, updateProductCategory, deleteProductCategory } from '../controllers/productCategoriesController.js';
 import { uploadProductImage, createProduct } from '../controllers/productsController.js';
+import { getAllOrderStates, createOrderState, updateOrderState, deleteOrderState } from '../controllers/orderStatesController.js';
 
 // Product Categories Endpoints
 router.get('/product-categories', getAllProductCategories);
 router.post('/product-categories', createProductCategory);
-router.delete('/product-categories/:id', deleteProductCategory);
 router.put('/product-categories/:id', updateProductCategory);
+router.delete('/product-categories/:id', deleteProductCategory);
 
 // Products Endpoints
 router.post('/products', uploadProductImage, createProduct);
+
+// Order States Endpoints
+router.get('/order-states', getAllOrderStates);
+router.post('/order-states', createOrderState);
+router.put('/order-states/:id', updateOrderState);
+router.delete('/order-states/:id', deleteOrderState);
 
 export default router;
