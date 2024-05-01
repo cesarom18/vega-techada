4cf8451e2be8ff2c0abcf36626e6d63cbeb4e41b
Cesar Osses <98192523+cesarom18@users.noreply.github.com>
2024-05-01 11:33:56 -0400

    Modificacion de pagina Error
    Se agrego propiedad de link hacia la pagina de inicio en el boton respectivo

--- front-end/src/error/pages/Error.jsx
+++ front-end/src/error/pages/Error.jsx
@@ -7,7 +7,9 @@ export const Error = () => {
         <h1 className='text-9xl font-black text-gray-800'>404</h1>
         <p className='text-2xl font-bold tracking-tight text-gray-700 sm:text-4xl'>Oops!</p>
         <p className='text-gray-500 mt-4 '>No pudimos encontrar la pagina que estas buscando.</p>
-        <Link className='btn btn-ghost bg-gray-800 hover:bg-gray-700 text-gray-50 mt-6 px-5 py-3 '>
+        <Link
+          to='/marketplace'
+          className='btn btn-ghost bg-gray-800 hover:bg-gray-700 text-gray-50 mt-6 px-5 py-3 '>
           Volver Al Inicio
         </Link>
       </div>
